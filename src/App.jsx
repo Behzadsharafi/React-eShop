b51fd8ae9e2ba8c4e1ded155b4bdb7e2a6e5d821
Behzad Sharafi <behzadsharafi@gmail.com>
2023-10-03 18:03:38 +1100

    cart-context needs to be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import { Header } from "./components/Header/Header";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-import { collection } from "firebase/firestore";
-import { db } from "./config/firestore";
-import ChildrenList from "./components/ChildrenList";
-import AddNew from "./components/AddNew";
 import CartPage from "./pages/CartPage/CartPage";
 import ItemsContextProvider from "./context/ItemsContextProvider";
 import CartContextProvider from "./context/CartContextProvider";
@@ -15,22 +10,8 @@ import ItemPage from "./pages/ItemPage/ItemPage";
 import styles from "./App.module.scss";
 
 function App() {
-  // const collectionRef = collection(db, "oses");
-  // const [docs, loading, error] = useCollectionData(collectionRef);
   return (
     <>
-      {/* <ul>
-        {docs?.map((doc) => (
-          <div key={Math.random()}>
-            <li>{doc.name}</li>
-
-            <ChildrenList path={`oses/${doc.name}/children`} />
-          </div>
-        ))}
-
-        <AddNew path="oses" />
-      </ul> */}
-
       <div className={styles.main}>
         <ItemsContextProvider>
           <CartContextProvider>
--- src/context/CartContextProvider.jsx
+++ src/context/CartContextProvider.jsx
@@ -9,30 +9,51 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedTotalAmount = state.totalAmount + action.item.price;
-    const existingCartItemIndex = state.cartItems.findIndex(
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-    const existingCartItem = state.cartItems[existingCartItemIndex];
+    const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
-        amount: existingCartItem.amount + 1,
+        amount: existingCartItem.amount + action.item.amount,
       };
-      updatedItems = [...state.cartItems];
+      updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
-      updatedItems = state.cartItems.concat(action.item);
+      updatedItems = state.items.concat(action.item);
     }
 
     return {
-      cartItems: updatedItems,
+      items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
 
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
@@ -41,25 +62,19 @@ const CartContextProvider = ({ children }) => {
     cartReducer,
     defaultCartState
   );
-  // const [cartItems, setCartItems] = useState([]);
-  // const [totalAmount, setTotalAmount] = useState(0);
 
-  // useEffect(() => {
-  //   setCartItems(cartItems);
-  // }, []);
-
-  const addItem = (item) => {
+  const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: "ADD", item: item });
   };
-  const removeItem = (id) => {
+  const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   const cartContext = {
-    cartItems: cartState.cartItems,
+    items: cartState.items,
     totalAmount: cartState.totalAmount,
-    addItem,
-    removeItem,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
   };
 
   return (
